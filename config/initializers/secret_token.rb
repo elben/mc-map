8ae6032557baf78c9bb13f8acbb9c29b02f0eb30
Elben Shira <elbenshira@gmail.com>
2013-08-02 22:40:32 -0500

    New rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+McMap::Application.config.secret_token = '4584214619dfea7afa646c9adf399ae95aff898f695e972137e15ed4289593a9dc3f0c333434e2e7a5d4d3b6c46934be0cbebdbcf989dafc857d3371ee9bec7e'
