10e4ab69eec5949c326ac19323b3f8d8b1eb71c8
Elben Shira <elbenshira@gmail.com>
2013-08-03 17:07:24 -0500

    Don't validate slug presence, since slug is added after save.

--- app/models/community.rb
+++ app/models/community.rb
@@ -6,7 +6,7 @@ class Community < ActiveRecord::Base
   attr_accessible :host_day, :host_kind, :description, :campus, :lat, :lng
   attr_accessible :slug, :deleted_at
 
-  validates :slug, :leader_first_name, presence: true
+  validates :leader_first_name, :leader_last_name, presence: true
 
   after_create :create_slug
   before_save :set_lat_lng
