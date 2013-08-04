1a0b204dd0b8419da054010a75a8c46e305a7ed2
Elben Shira <elbenshira@gmail.com>
2013-08-03 22:33:32 -0500

    Set correct config.

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module McMap
     config.assets.enabled = true
 
     # Required false by Heroku
-    config.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
