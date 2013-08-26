72c8b06f804567026c16bbe35009b4917e545cd5
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2013-08-25 22:46:28 -0500

    Refactor email subjects
    Leaders now get the member's name in the subject instead of a generic one,
    signer-uppers get more capital letters.

--- app/mailers/member_sign_up_mailer.rb
+++ app/mailers/member_sign_up_mailer.rb
@@ -6,13 +6,13 @@ class MemberSignUpMailer < ActionMailer::Base
     @member = member
     @community = community
     reply_to = @community.email || "connections@ministries.austinstone.rog"
-    mail(reply_to: reply_to, to: member.email, subject: "Austin Stone missional community confirmation")
+    mail(reply_to: reply_to, to: member.email, subject: "Austin Stone Missional Community Confirmation")
   end
 
   # Sent to leaders of MC after new member
   def leaders_email(member, community)
     @member = member
     @community = community
-    mail(to: @community.email, subject: "Someone signed up for your MC")
+    mail(to: @community.email, subject: "#{member.name} signed up for your MC!")
   end
 end
