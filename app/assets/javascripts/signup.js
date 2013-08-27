8c7abba7fb02d39e3ab98b11678d03f7c94af082
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2013-08-27 00:00:38 -0500

    Refactor buttons/form JS, add form reset button

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -3,50 +3,77 @@
 (function () {
   var $form = $('#signup-form');
   var $submit = $form.find('input[type="submit"]');
-  var $name = $form.find('#name');
-  var $email = $form.find('#email');
-  var $phoneNumber = $form.find('#phone-number');
+  var $reset = $form.find('#form-reset-button');
+
+  // all user-editable inputs
+  var $inputs = $form.find('input:visible:not([type="submit"])');
 
   var loadingClass = 'loading';
+  var storageKeyPrefix = 'sign-up-input-value:';
+  var storageKeyRegexp = new RegExp(storageKeyPrefix + '#([a-zA-Z0-9_-]+)');
 
-  // store use info locally whenever they successfully submit the form
-  if (window.localStorage) {
-    // the key prefix we store the information under
-    var keyPrefix = 'sign-up-info-';
-
-    // store the info in localStorage when the form is submitted
-    $form.on('submit', function () {
-      var name = $name.val();
-      var email = $email.val();
-      var phoneNumber = $phoneNumber.val();
-
-      window.localStorage.setItem(keyPrefix + 'name', name);
-      window.localStorage.setItem(keyPrefix + 'email', email);
-      window.localStorage.setItem(keyPrefix + 'phone-number', phoneNumber);
-    });
-
-    // load the info from localstorage into the form if it exists
-    var name = window.localStorage.getItem(keyPrefix + 'name');
-    var email = window.localStorage.getItem(keyPrefix + 'email');
-    var phoneNumber = window.localStorage.getItem(keyPrefix + 'phone-number');
-
-    $name.val(name);
-    $email.val(email);
-    $phoneNumber.val(phoneNumber);
-  }
-
-  // mark the form as 'loading' when the submit button is clicked, but disable
-  // it if any of the elements was invalid.
-  $submit.on('click', function (e) { $form.addClass(loadingClass); });
-
+  // stop the loading spinners when the form is submitted, but invalid
   var handleInvalidForm = function (e) {
     $form.removeClass(loadingClass);
   };
 
-  $name.on('invalid', handleInvalidForm);
-  $email.on('invalid', handleInvalidForm);
-  $phoneNumber.on('invalid', handleInvalidForm);
+  // show the loading animation until it's canceled or the form submits
+  var handleSubmitClick = function () {
+    $form.addClass(loadingClass);
+  };
+
+  // reset all inputs when the reset button is clicked
+  var handleResetClick = function (e) {
+    e.preventDefault();
+
+    // kill the loading class, just for kicks
+    $form.removeClass(loadingClass);
+
+    $inputs.val('');
+  };
+
+  // get the name of a key to store input information under
+  var buildStorageKey = function (id) {
+    return storageKeyPrefix + '#' + id;
+  };
+
+  // store all input values to localStorage if possible
+  var storeInputValues = function () {
+    if (window.localStorage) {
+      // store each value by its input's id
+      $inputs.each(function () {
+        var $input = $(this);
+        var id = $input.attr('id');
+        var value = $input.val();
+
+        window.localStorage.setItem(buildStorageKey(id), value);
+      });
+    }
+  };
+
+  // load all input values from localStorage if possible
+  var loadInputValues = function () {
+    if (window.localStorage) {
+      // store each value by its input's id
+      $inputs.each(function () {
+        var $input = $(this);
+        var id = $input.attr('id');
+
+        // load the value and give it to the input
+        var value = window.localStorage.getItem(buildStorageKey(id));
+        if (value) { $input.val(value); }
+      });
+    }
+  };
+
+  // store the info in localStorage when the form is submitted
+  $form.on('submit', storeInputValues);
+  loadInputValues();
+
+  $reset.on('click', handleResetClick);
+  $submit.on('click', handleSubmitClick);
+  $inputs.on('invalid', handleInvalidForm);
 
   // focus the first visible input
-  $form.find('input:visible').first().trigger('focus');
+  $inputs.first().trigger('focus');
 }());
