0c1884cb8c35e40ac66789753a56058478749dc5
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2013-08-31 13:14:55 -0500

    Update title

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Austin Stone MC Map"
+  config.site_title = "Missional Community Finder | The Austin Stone Community Church"
 
   config.authorization_adapter = "LeadersAuthorization"
 
