9814f4fad3f094de69dfbc6f0842299fcdeede25
Elben Shira <elbenshira@gmail.com>
2013-08-31 14:24:01 -0500

    Configs for caching.
    - Enable caching in dev mode.
    - Enable memcache caching in production using dalli.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,8 @@ McMap::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
+  config.cache_store = :memory_store, {:size => 4194304} # bytes; 4 MB
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
