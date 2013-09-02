68701367fdb697237a9638c05587017dcd999d66
Elben Shira <elbenshira@gmail.com>
2013-09-01 23:52:30 -0500

    Require login for admin2.

--- app/controllers/admin2/admin2_controller.rb
+++ app/controllers/admin2/admin2_controller.rb
@@ -1,6 +1,8 @@
 class Admin2::Admin2Controller < ApplicationController
   layout 'admin2'
 
+  before_filter :admin_login_required
+
   def index
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def admin_login_required
+    return if current_admin_user
+    redirect_to admin_root_path
+  end
 end
