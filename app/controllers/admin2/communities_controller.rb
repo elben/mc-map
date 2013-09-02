ae48d762a6c894569927c450de0c0580ae3f7a79
Elben Shira <elbenshira@gmail.com>
2013-09-01 23:57:35 -0500

    Speed up admin2/communities

--- app/controllers/admin2/communities_controller.rb
+++ app/controllers/admin2/communities_controller.rb
@@ -1,6 +1,14 @@
 class Admin2::CommunitiesController < Admin2::Admin2Controller
   def index
     @communities = Community.all
+
+    @members_count = CommunitiesMembers.group(:community_id).where(:community_id => @communities).count
+    @taggings_hash = {}
+    taggings = ActsAsTaggableOn::Tagging.where(:taggable_id => @communities, :taggable_type => Community, :context => "kinds").includes(:tag)
+    taggings.each do |tagging|
+      @taggings_hash[tagging.taggable_id] ||= []
+      @taggings_hash[tagging.taggable_id] << tagging.tag
+    end
   end
 
   def show
