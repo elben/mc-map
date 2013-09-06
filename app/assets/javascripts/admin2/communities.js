8a08134e1988504f29d7ce52e8eb4475f66a9c93
Elben Shira <elbenshira@gmail.com>
2013-09-05 23:52:10 -0500

    Changing sheets for community.

--- app/assets/javascripts/admin2/communities.js
+++ app/assets/javascripts/admin2/communities.js
@@ -2,4 +2,34 @@ $(function () {
   var communitiesView = new AdminViews.CommunitiesView({
     el: $("#communities"),
   });
+
+  var CommunityView = Backbone.View.extend({
+    events: {
+      "click li[data-sheet] a": "handleSheetChange",
+    },
+    sheets: null, // Name of sheets
+    currentSheetName: null,
+    initialize: function (options) {
+      this.sheets = $("li [data-sheet]").map(function (idx, li) { return $(li).data("sheet"); });
+
+      this.changeSheet("info"); // Defaults to Info sheet
+    },
+    handleSheetChange: function (e) {
+      var toSheetName = $(e.target).parent().data("sheet");
+      this.changeSheet(toSheetName);
+    },
+    changeSheet: function (toSheetName) {
+      this.$el.find("a[data-sheet='" + this.currentSheetName + "']").parent().removeClass("active");
+      this.$el.find(".sheet[data-sheet='" + this.currentSheetName + "']").hide()
+
+      this.$el.find("a[data-sheet='" + toSheetName + "']").parent().addClass("active");
+      this.$el.find(".sheet[data-sheet='" + toSheetName + "']").show()
+
+      this.currentSheetName = toSheetName;
+    }
+  });
+
+  var communityView = new CommunityView({
+    el: $("#community")
+  });
 });
