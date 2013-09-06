c1bf43c392d4e1b27b95b55c680adc19ef865bb5
Elben Shira <elbenshira@gmail.com>
2013-09-06 17:46:26 -0500

    Add flash messages and alert dismissals.

--- app/assets/javascripts/admin2.js
+++ app/assets/javascripts/admin2.js
@@ -1,4 +1,5 @@
 //= require jquery
+//= require bootstrap-alert
 //= require underscore
 //= require backbone
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  # Public: Converts Rails flash name to Bootstrap class.
+  #
+  # flash_name - Rails flash name. Use "flash", "notice" or "alert".
+  #
+  # Returns CSS class name.
+  def bootstrap_alert_class(flash_name)
+    case flash_name.to_s
+    when "flash"
+      return "alert-info"
+    when "notice"
+      return "alert-success"
+    when "alert"
+      return "alert-error"
+    end
+  end
 end
