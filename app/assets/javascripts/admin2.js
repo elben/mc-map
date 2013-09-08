b9df6b9e01c39f43e8d8e3bd08300bb109dc7f63
Elben Shira <elbenshira@gmail.com>
2013-09-07 23:28:48 -0500

    datatables for communities.

--- app/assets/javascripts/admin2.js
+++ app/assets/javascripts/admin2.js
@@ -3,6 +3,7 @@
 //= require underscore
 //= require backbone
 //= require jquery.dataTables.min
+//= require dataTables_bootstrap_paging
 
 var AdminViews = {};
 
@@ -39,11 +40,32 @@ $(function () {
 
   AdminViews.CommunitiesView = Backbone.View.extend({
     navBarView: null,
+    table: null,
 
     initialize: function (options) {
       this.navBarView = buildNavBarView("communities");
-      $("table.communities").dataTable({
-        "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>"
+      this.table = $("table.communities");
+      this.table.dataTable({
+        // sDom and sPaginationType follows:
+        // http://datatables.net/blog/Twitter_Bootstrap_2
+        "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+        "sPaginationType": "bootstrap", // Depends on dataTables_bootstrap_pagination.js
+        "oLanguage": {
+          "sLengthMenu": "Display _MENU_ records",
+          "sInfo": "Showing _TOTAL_ _START_ to _END_ of _TOTAL_ communities",
+          "sInfoEmpty": "No communities to show",
+        },
+        "bLengthChange": false, // Hide num rows dropdown
+        // "bFilter": false, // Hide search bar since we're building our own
+        "iDisplayLength": 50, // Display 50 rows at a time
+        "aaSorting": [[ 1, "asc" ]], // Default to sort first column
+        "aoColumns": [ {bSortable: false, bSearchable: false}, {}, {}, {}, {}, {}, {bSearchable: false} ]
+      });
+
+      $("#DataTables_Table_0_filter").hide();
+      var self = this;
+      $('#communities-search').keyup(function(){
+        self.table.fnFilter( $(this).val() );
       });
     }
   });
